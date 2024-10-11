4e983534fde095b60cbfa709374f843e31a225ca
Fish-SWA <zhonghengyu2@gmail.com>
2024-10-12 05:23:50 +0800

    init version

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <Arduino.h>
+
+
+TaskHandle_t ENS_HandleTask = NULL;
+TaskHandle_t LED_HandleTask = NULL;
+
+int system_init()
+{
+    
+  return 0;
+}
+
+
+void setup() {
+  system_init();
+}
+
+void loop() {
+  // digitalWrite(LED_PIN, HIGH);
+  // delay(1000);
+  // digitalWrite(LED_PIN, LOW);
+  // delay(1000);
+  // printf("ok\n");
+}
