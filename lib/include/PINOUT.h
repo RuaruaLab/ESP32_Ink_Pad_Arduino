5ff8dbc4a876d65d961dcf52d968c5f062b245d0
FishSWA <fishswaa@gmail.com>
2024-10-12 12:29:29 +0800

    点亮了墨水屏

---
+++ lib/include/PINOUT.h
@@ -0,0 +1,17 @@
+#ifndef PINOUT_H
+#define PINOUT
+
+
+#define LED_PIN 2
+#define ADC_IN 3
+#define V_3125_PIN 15
+#define INK_MOSI 23
+#define INK_CLK 22
+#define INK_CS 21
+#define INK_DC 20
+#define INK_RST 19
+#define INK_BUSY 18
+#define IIC_SCL 4
+#define IIC_SDA 5
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,41 @@
 #include <Arduino.h>
-
+#include "PINOUT.h"
+#include "EPD_2in13g.h"
+#include "imagedata.h"
 
 TaskHandle_t ENS_HandleTask = NULL;
 TaskHandle_t LED_HandleTask = NULL;
 
+Epd epd;
+
 int system_init()
 {
-    
+  pinMode(LED_PIN, OUTPUT);
+
+  epd.Init();
   return 0;
 }
 
 
 void setup() {
-  system_init();
+  if(system_init() != 0)
+  {
+    printf("e-Paper init fail!\n");
+  }else{
+    printf("e-paper init ok\n");
+  }
+  epd.Display(gImage_2in13g);
 }
 
 void loop() {
-  // digitalWrite(LED_PIN, HIGH);
-  // delay(1000);
-  // digitalWrite(LED_PIN, LOW);
-  // delay(1000);
-  // printf("ok\n");
+  // epd.Clear(WHITE);
+  // delay(2000);
+
+  // epd.Display(gImage_2in13g);
+  // delay(2000);
+
+  // epd.Clear(WHITE);
+  // delay(2000);
+
+  epd.Sleep();
 }
