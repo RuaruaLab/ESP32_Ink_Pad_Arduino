d27b18cd18542b2dc3be861e7ea9789f98c1f22f
FishSWA <fishswaa@gmail.com>
2024-10-12 13:36:17 +0800

    add changle pixel cmd

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,41 @@ TaskHandle_t LED_HandleTask = NULL;
 
 Epd epd;
 
+/*图片缓冲区*/
+/******像素定义*******
+* 单个像素：
+* 0x00 : 黑
+* 0x01 : 白
+* 0x02 : 黄
+* 0x03 : 红
+* 扫描方式：从下往上，从左往右
+* 屏幕分辨率：124(rol)*250(col)
+*/
+uint8_t IMG_buffer[7750];
+#define PIXEL_BLACK   0x00
+#define PIXEL_WHITE   0X01
+#define PIXEL_YELLOW  0x02
+#define PIXEL_RED     0x03
+
+//设定任意个像素的颜色
+// 设置指定位置像素的颜色
+void img_pixel_set(uint8_t *img, int rol, int col, uint8_t color)
+{
+    //越界检查
+    if(rol < 0 || rol >= 124 || col < 0 || col >= 250) {
+        return;
+    }
+    //计算索引
+    int index = rol + col * 124;
+    int byte_index = index / 4;
+    // int bit_offset = (index % 4) * 2;
+    int bit_offset = 0;
+    
+    //设定颜色
+    img[byte_index] &= ~(0x03 << bit_offset);
+    img[byte_index] |= (color << bit_offset);
+}
+
 int system_init()
 {
   pinMode(LED_PIN, OUTPUT);
@@ -24,7 +59,24 @@ void setup() {
   }else{
     printf("e-paper init ok\n");
   }
-  epd.Display(gImage_2in13g);
+
+  /*设置为全白*/
+  int pixel_vel;
+  for(int i=0; i<sizeof(IMG_buffer); i++){
+    pixel_vel = PIXEL_WHITE;
+    IMG_buffer[i] = pixel_vel << 6 | pixel_vel << 4 | pixel_vel << 2 | pixel_vel;
+  }
+
+  /*画一个y=x的线*/
+  for(int i=0; i<124; i++){
+    img_pixel_set(IMG_buffer, i, i*2, PIXEL_RED);
+  }
+
+  // IMG_buffer[50] = pixel_vel << 6 | pixel_vel << 4 | pixel_vel << 2 | pixel_vel;
+
+  // img_pixel_set(IMG_buffer, 50, 50, PIXEL_RED);
+
+  epd.Display(IMG_buffer);
 }
 
 void loop() {
