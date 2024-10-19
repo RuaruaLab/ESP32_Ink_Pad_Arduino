276544f0e2a9399410808f204d34a67715d65f71
Fish-SWA <zhonghengyu2@gmail.com>
2024-10-20 04:15:58 +0800

    fefresh fix

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,17 @@
 #include "PINOUT.h"
 #include "display.hpp"
 
+/*系统状态*/
+enum SystemStatus {
+    NORMAL = 0x00,
+    USB_EXTRACT = 0x01
+};
+uint8_t sys_status = NORMAL;
+
 void usb_extract_handle()
 {
-  display.refreshScreen();
-  while (1){}
+  if(digitalRead(V_3125_PIN) == LOW) sys_status = USB_EXTRACT;
+  else if(digitalRead(V_3125_PIN) == HIGH) sys_status = NORMAL;
 }
 
 void setup() {
@@ -15,7 +22,7 @@ void setup() {
     digitalWrite(LED_PIN, LOW);
     //USB拔出检测
     pinMode(V_3125_PIN, INPUT);
-    attachInterrupt(V_3125_PIN, usb_extract_handle, FALLING);
+    attachInterrupt(V_3125_PIN, usb_extract_handle, CHANGE);
     /*墨水屏初始化*/
     display.begin();
     display.clearScreen(EPaperDisplay::PIXEL_WHITE);
@@ -24,7 +31,20 @@ void setup() {
         display.setPixel(i, i, EPaperDisplay::PIXEL_RED);
 
     }
+    // display.refreshScreen();
+    // display.set(gImage_2in13g);
 }
 
 void loop() {
+  /*USB拔出处理*/
+  if(sys_status == USB_EXTRACT)
+  {
+    display.set(gImage_2in13g);
+    while(1)
+    {
+      delay(100);
+      if(sys_status == NORMAL) break;
+    }
+  }
+  
 }
