4b1593556dd52ffc50d76c11c1c93e84332a7bbb
Fish-SWA <zhonghengyu2@gmail.com>
2024-10-23 08:36:57 +0800

    可靠的串口接收数据包

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,8 @@
 #include "PINOUT.h"
 #include "display.hpp"
 
+uint8_t recive_buff[256] = {0};
+
 /*系统状态*/
 enum SystemStatus {
     NORMAL = 0x00,
@@ -15,6 +17,8 @@ void usb_extract_handle()
   else if(digitalRead(V_3125_PIN) == HIGH) sys_status = NORMAL;
 }
 
+void serial_ubpack(uint8_t *pack);
+
 void setup() {
     /*GPIO初始化*/
     //LED
@@ -22,7 +26,7 @@ void setup() {
     digitalWrite(LED_PIN, LOW);
     //USB拔出检测
     pinMode(V_3125_PIN, INPUT);
-    attachInterrupt(V_3125_PIN, usb_extract_handle, CHANGE);
+    // attachInterrupt(V_3125_PIN, usb_extract_handle, CHANGE);
     /*墨水屏初始化*/
     display.begin();
     display.clearScreen(EPaperDisplay::PIXEL_WHITE);
@@ -37,14 +41,50 @@ void setup() {
 
 void loop() {
   /*USB拔出处理*/
-  if(sys_status == USB_EXTRACT)
-  {
-    display.set(gImage_2in13g);
-    while(1)
+  // if(sys_status == USB_EXTRACT)
+  // {
+  //   display.set(gImage_2in13g);
+  //   while(1)
+  //   {
+  //     delay(100);
+  //     if(sys_status == NORMAL) break;
+  //   }
+  // }
+
+  /*
+  通讯协议：
+  第一位  帧头 0x5C
+  第二位  数据长度，不含帧头(256以内)
+  第三位  CMD_ID
+  <数据段>
+  末位    帧尾 0x7A
+  */
+  /*接收数据包*/
+  if(Serial.available() > 0){
+    /*识别帧头*/
+    uint8_t read_data;
+    read_data = Serial.read();
+    if(read_data == 0x5C)
     {
-      delay(100);
-      if(sys_status == NORMAL) break;
+      memset(recive_buff, 0, sizeof(recive_buff));  //清零缓冲区
+
+      uint8_t data_length = 0;
+      data_length = Serial.read();
+      Serial.readBytes(recive_buff, data_length);
+      if(recive_buff[data_length-1] == 0x7A)
+      {
+        serial_ubpack(recive_buff);
+      }
     }
   }
+
+  // printf("main task ok\n");
+  delay(1);
   
+}
+
+void serial_ubpack(uint8_t *pack)
+{
+  printf("%s\n", pack);
+  return ;
 }
